1ba167cef2473a7d95674650308e9e17681e47d2
kimboseong <njunju77@nate.com>
2024-07-28 07:47:51 +0900

    init commit

---
+++ src/main/java/com/resttemplate_server/RestTemplateServerApplication.java
@@ -0,0 +1,13 @@
+package com.resttemplate_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateServerApplication.class, args);
+    }
+
+}
